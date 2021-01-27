bd24c5c48bb78d3a9de0f65486187abaf9f2df09
wangxin-jm <wangxinjiangming@126.com>
2021-01-27 21:38:42 +0800

    规格参数组增删改查

---
+++ mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/SpecParamService.java
@@ -0,0 +1,41 @@
+package com.baidu.shop.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.SpecParamDTO;
+import com.baidu.shop.entity.SpecParamEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 2 * @ClassName SpecParamService
+ * 3 * @Description: TODO
+ * 4 * @Author wangxin
+ * 5 * @Date 2021/1/27
+ * 6 * @Version V1.0
+ * 7
+ **/
+@Api(tags = "规格参数接口")
+public interface SpecParamService {
+
+    @ApiOperation("规格参数查询")
+    @GetMapping("specParam/list")
+    Result<SpecParamEntity> list(SpecParamDTO specParamDTO);
+
+    @ApiOperation("规格参数新增")
+    @PostMapping("specParam/save")
+    Result<JSONObject> save(@Validated({MingruiOperation.Add.class})@RequestBody SpecParamDTO specParamDTO);
+
+    @ApiOperation("规格参数修改")
+    @PutMapping("specParam/save")
+    Result<JSONObject> update(@Validated({MingruiOperation.Update.class})@RequestBody SpecParamDTO specParamDTO);
+
+    @ApiOperation("删除规格参数")
+    @DeleteMapping("specParam/delete")
+    Result<JSONObject> delete(Integer id);
+
+
+}
---
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/mapper/SpenParamMapper.java
@@ -0,0 +1,9 @@
+package com.baidu.shop.mapper;
+
+import com.baidu.shop.entity.SpecParamEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SpenParamMapper extends Mapper<SpecParamEntity> {
+
+
+}
